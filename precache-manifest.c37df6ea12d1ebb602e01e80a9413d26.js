f7c53b4d439fa14f148119fadba1f35386b4dd5f
Udit Sahani <uditrsahani@gmail.com>
2022-06-04 19:07:40 +0700

    Updates

---
+++ precache-manifest.c37df6ea12d1ebb602e01e80a9413d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f7c4aeb41a012e8ee4b",
+    "url": "/online-store-reactjs/static/css/main.b0a401a7.chunk.css"
+  },
+  {
+    "revision": "8f7c4aeb41a012e8ee4b",
+    "url": "/online-store-reactjs/static/js/main.8f7c4aeb.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/online-store-reactjs/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "17487a250e7dac077d50",
+    "url": "/online-store-reactjs/static/js/2.17487a25.chunk.js"
+  },
+  {
+    "revision": "5f1ff6b9e2a0ecf4cb44",
+    "url": "/online-store-reactjs/static/js/runtime~main.5f1ff6b9.js"
+  },
+  {
+    "revision": "2e4d6bd09a0c7ce83e21f49745b3e44f",
+    "url": "/online-store-reactjs/index.html"
+  }
+];
